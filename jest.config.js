d25e0475e32105e1591879c8dee164a88b98addf
briancldo <briancldo@gmail.com>
2021-03-19 18:47:35 -0400

    chore: init repo

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
