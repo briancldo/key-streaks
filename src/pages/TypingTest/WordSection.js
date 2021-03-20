b71689cf73d08489d397bc0498f84b08cd4aa3aa
briancldo <briancldo@gmail.com>
2021-03-19 23:40:07 -0400

    feat: highlights current word

--- src/pages/TypingTest/WordSection.js
+++ src/pages/TypingTest/WordSection.js
@@ -11,13 +11,16 @@ export default function WordSection(props) {
     words.slice(0, currentWordIndex)
   );
   const incompleteWordsString = getWordsStringFromArray(
-    words.slice(currentWordIndex)
+    currentWordIndex + 1 === words.length
+      ? []
+      : words.slice(currentWordIndex + 1)
   );
 
   return (
     <Card>
       <p>
         <b>{completedWordsString}</b>&nbsp;
+        <b style={{ color: 'red' }}>{words[currentWordIndex]}</b>&nbsp;
         {incompleteWordsString}
       </p>
     </Card>
