d8b2aa6d673acaa3dc99f36012037682988cd32b
briancldo <briancldo@gmail.com>
2021-03-19 23:15:30 -0400

    feat: typing test validation works with harded words

--- src/pages/TypingTest/InputSection.js
+++ src/pages/TypingTest/InputSection.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 
-export default function InputSection() {
-  return <TextField />;
+export default function InputSection(props) {
+  const { currentWord, finishCurrentWord, disabled } = props;
+  const [currentEntry, setCurrentEntry] = useState('');
+
+  useEffect(() => {
+    console.log('new word');
+    setCurrentEntry('');
+  }, [currentWord]);
+
+  function checkLetter(event) {
+    const value = event.target.value;
+
+    if (value === currentWord) finishCurrentWord();
+    if (!currentWord.startsWith(value)) finishCurrentWord(false);
+
+    setCurrentEntry(value);
+  }
+
+  return (
+    <TextField
+      value={currentEntry}
+      onChange={checkLetter}
+      placeholder={currentWord}
+      disabled={disabled}
+    />
+  );
 }
--- src/pages/TypingTest/TestSection.js
+++ src/pages/TypingTest/TestSection.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Input from './InputSection';
 import WordSection from './WordSection';
 
+const GAME_STATUSES = {
+  ongoing: undefined,
+  won: 'You won!',
+  lost: 'You lost!',
+};
+
 export default function TestSection() {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const words = ['bdo', 'is', 'the', 'goat'];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentWord, setCurrentWord] = useState(words[0]);
+  const [gameStatus, setGameStatus] = useState(GAME_STATUSES.ongoing);
+
+  useEffect(() => {
+    try {
+      let word = words[currentWordIndex];
+      if (currentWordIndex < words.length - 1) word = `${word} `;
+      setCurrentWord(word);
+    } catch {
+      throw new Error('No more words.');
+    }
+  }, [currentWordIndex, words]);
+
+  function finishCurrentWord(passed = true) {
+    if (!passed) return setGameStatus(GAME_STATUSES.lost);
+
+    setCurrentWordIndex((index) => {
+      if (index + 1 === words.length) {
+        setGameStatus(GAME_STATUSES.won);
+        return index;
+      }
+
+      return index + 1;
+    });
+  }
 
   return (
     <div>
       <WordSection words={words} />
-      <Input />
+      <Input
+        currentWord={currentWord}
+        finishCurrentWord={finishCurrentWord}
+        disabled={gameStatus !== GAME_STATUSES.ongoing}
+      />
+      <p>{gameStatus}</p>
     </div>
   );
 }
