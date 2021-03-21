0acf589065b7c876177809ec4150ce57a9dfbe3c
briancldo <briancldo@gmail.com>
2021-03-20 23:10:26 -0400

    feat: adds basic styling to the leaderboard

--- src/pages/TypingTest/Leaderboard.js
+++ src/pages/TypingTest/Leaderboard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
+import { useStyles } from './Leaderboard.styles';
 
 export default function Leaderboard(props) {
   const { leaderboard } = props;
+  const styles = useStyles();
 
   return (
-    <Card>
-      <h3>Leaderboard</h3>
+    <Card className={styles.leaderboard}>
+      <Typography variant='h5'>Leaderboard</Typography>
       {leaderboard.map((entry, index) => (
         <p key={`personalLeaderboard${index + 1}`}>
           <b>{index + 1})</b> {entry.score}
