89e638ce8982b98871afc1818a1bd83b8af4a0c9
briancldo <briancldo@gmail.com>
2021-03-20 22:53:30 -0400

    feat: adds function to add score to leaderboard

--- src/utils/leaderboard.js
+++ src/utils/leaderboard.js
@@ -1,16 +1,32 @@
-// const MAX_SCORES = 10;
+const MAX_SCORES = 10;
 const leaderboardKey = 'personalLeaderboard';
+
+function validateLeaderboard(leaderboard) {
+  if (!Array.isArray(leaderboard)) throw new Error('noarr');
+
+  for (const place of leaderboard) {
+    if (typeof place.score !== 'number') throw new Error('nonum');
+  }
+}
 
 function getLeaderboard() {
   try {
     const leaderboardString = localStorage.getItem(leaderboardKey) || '[]';
-    return JSON.parse(leaderboardString);
+    const leaderboard = JSON.parse(leaderboardString);
+    validateLeaderboard(leaderboard);
+
+    return leaderboard;
   } catch {
     localStorage.setItem(leaderboardKey, '[]');
     return [];
   }
 }
 
-// function addScoreIfQualified(score) {}
+function addScoreIfQualified(score, leaderboard = getLeaderboard()) {
+  const newLeaderboard = [...leaderboard, { score }]
+    .sort((a, b) => (a.score < b.score ? 1 : -1))
+    .slice(0, MAX_SCORES);
+  localStorage.setItem(leaderboardKey, JSON.stringify(newLeaderboard));
+}
 
-export { getLeaderboard };
+export { getLeaderboard, addScoreIfQualified };
