e41d0378586b7c94f47e8d565ab498b0d783f914
briancldo <briancldo@gmail.com>
2021-03-25 18:37:46 -0400

    feat: transforms leaderboard into a table

--- src/pages/TypingTest/Leaderboard.js
+++ src/pages/TypingTest/Leaderboard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 import { useStyles } from './Leaderboard.styles';
 
@@ -11,11 +17,24 @@ export default function Leaderboard(props) {
   return (
     <Card className={styles.leaderboard}>
       <Typography variant='h5'>Leaderboard</Typography>
-      {leaderboard.map((entry, index) => (
-        <p key={`personalLeaderboard${index + 1}`}>
-          <b>{index + 1})</b> {entry.streak}
-        </p>
-      ))}
+      <TableContainer component={Card}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Rank</TableCell>
+              <TableCell>Streak</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {leaderboard.map((entry, index) => (
+              <TableRow key={`personalLeaderboardRank${index + 1}`}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{entry.streak}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Card>
   );
 }
